7a6464a16b5272795abdf3b7dfb8488dcfc2964b
hiteshKumarMoneyFactory <113467512+hiteshKumarMoneyFactory@users.noreply.github.com>
2025-05-03 17:08:01 +0530

    adding comments done

--- client/src/features/experiences/components/ExperienceCard.tsx
+++ client/src/features/experiences/components/ExperienceCard.tsx
@@ -3,6 +3,7 @@ import { LinkIcon, MessageSquare } from "lucide-react";
 import Card from "@/features/shared/components/ui/Card";
 
 import { ExperienceForList } from "../types";
+import { CommentsSection } from "@/features/comments/components/CommentsSection";
 
 type ExperienceCardProps = {
   experience: ExperienceForList;
@@ -17,6 +18,9 @@ export function ExperienceCard({ experience }: ExperienceCardProps) {
         <ExperienceCardContent experience={experience} />
         <ExperienceCardMeta experience={experience} />
         <ExperienceCardMetricButtons experience={experience} />
+        <CommentsSection experienceId={experience.id}
+        commentsCount={experience.commentsCount}
+        />
       </div>
     </Card>
   );
