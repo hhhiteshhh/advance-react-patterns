e50726303f4ebcd4dd5a4cb43b37ded4d91ed453
hiteshKumarMoneyFactory <113467512+hiteshKumarMoneyFactory@users.noreply.github.com>
2025-05-04 19:04:52 +0530

    handle errors

--- client/src/features/comments/components/CommentsSection.tsx
+++ client/src/features/comments/components/CommentsSection.tsx
@@ -2,6 +2,7 @@ import { Experience } from "@advanced-react/server/features/experience/models";
 import { trpc } from "@/router";
 import CommentList from "./CommentList";
 import { CommentCreateForm } from "./CommentCreateForm";
+import { ErrorComponent } from "@/features/shared/components/ErrorComponent";
 
 type CommentsSectionProps = {
       experienceId: Experience["id"];
@@ -20,7 +21,7 @@ export function CommentsSection({
       );
     
       if (commentsQuery.error) {
-        return <div>Something went wrong</div>;
+        return <ErrorComponent />
       }
     
       return (
--- client/src/router.tsx
+++ client/src/router.tsx
@@ -5,57 +5,58 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import Spinner from "@/features/shared/components/ui/Spinner";
+import { ErrorComponent } from "@/features/shared/components/ErrorComponent";
 
-export const queryClient =new QueryClient()
+export const queryClient = new QueryClient()
 
 export const trpc = createTRPCReact<AppRouter>();
-export const trpcClient = 
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: env.VITE_SERVER_BASE_URL,
-        }),
-      ],
-    })
-  
-    export const trpcQueryUtils = createTRPCQueryUtils({
-          queryClient,
-          client: trpcClient,
-    });
-        
-    function createRouter() {
-          const router = createTanStackRouter({
-            routeTree,
-            defaultPreload: "intent",
-            scrollRestoration: true,
-            context: {
-              trpcQueryUtils,
-            },
-                defaultPendingComponent: () => (
-                    <div className="flex items-center justify-center">
-                      <Spinner />
-                    </div>
-                  ),
-              
-            Wrap: function WrapComponent({ children }) {
-              return (
-                <trpc.Provider client={trpcClient} queryClient={queryClient}>
-                  <QueryClientProvider client={queryClient}>
-                    {children}
-                  </QueryClientProvider>
-                </trpc.Provider>
-              );
-            },
-          });
-        
-          return router;
-        }
-        
-        export const router = createRouter();
-        
-        declare module "@tanstack/react-router" {
-          interface Register {
-            router: ReturnType<typeof createRouter>;
-          }
-        }
-        
+export const trpcClient =
+  trpc.createClient({
+    links: [
+      httpBatchLink({
+        url: env.VITE_SERVER_BASE_URL,
+      }),
+    ],
+  })
+
+export const trpcQueryUtils = createTRPCQueryUtils({
+  queryClient,
+  client: trpcClient,
+});
+
+function createRouter() {
+  const router = createTanStackRouter({
+    routeTree,
+    defaultPreload: "intent",
+    scrollRestoration: true,
+    context: {
+      trpcQueryUtils,
+    },
+    defaultPendingComponent: () => (
+      <div className="flex items-center justify-center">
+        <Spinner />
+      </div>
+    ),
+    defaultErrorComponent: ErrorComponent,
+
+    Wrap: function WrapComponent({ children }) {
+      return (
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </trpc.Provider>
+      );
+    },
+  });
+
+  return router;
+}
+
+export const router = createRouter();
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof createRouter>;
+  }
+}
