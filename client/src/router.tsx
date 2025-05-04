1278a03bfcb023070354199e87c2f87705e9d853
hiteshKumarMoneyFactory <113467512+hiteshKumarMoneyFactory@users.noreply.github.com>
2025-05-04 19:26:45 +0530

    not found page added

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,12 @@
 import type { AppRouter } from "@advanced-react/server";
-import { createTRPCQueryUtils, createTRPCReact, httpBatchLink } from "@trpc/react-query";
+import { createTRPCQueryUtils, createTRPCReact, httpBatchLink, TRPCClientError } from "@trpc/react-query";
 import { env } from "./lib/utils/env";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import Spinner from "@/features/shared/components/ui/Spinner";
 import { ErrorComponent } from "@/features/shared/components/ErrorComponent";
+import { NotFoundComponent } from "./features/shared/components/NotFoundComponent";
 
 export const queryClient = new QueryClient()
 
@@ -38,6 +39,7 @@ function createRouter() {
       </div>
     ),
     defaultErrorComponent: ErrorComponent,
+    defaultNotFoundComponent: NotFoundComponent,
 
     Wrap: function WrapComponent({ children }) {
       return (
@@ -59,4 +61,11 @@ declare module "@tanstack/react-router" {
   interface Register {
     router: ReturnType<typeof createRouter>;
   }
+}
+
+
+export function isTRPCClientError(
+  cause: unknown,
+): cause is TRPCClientError<AppRouter> {
+  return cause instanceof TRPCClientError;
 }
