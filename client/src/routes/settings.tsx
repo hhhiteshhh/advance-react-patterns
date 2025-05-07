2b575787da397bc0b3f38b444ca19a726e5492fe
hiteshKumarMoneyFactory <113467512+hiteshKumarMoneyFactory@users.noreply.github.com>
2025-05-07 23:18:40 +0530

    change email functionality added

--- client/src/routes/settings.tsx
+++ client/src/routes/settings.tsx
@@ -4,67 +4,75 @@ import { Button } from "@/features/shared/components/ui/Button";
 import Card from "@/features/shared/components/ui/Card";
 import { useToast } from "@/features/shared/hooks/useToast";
 import { router, trpc } from "@/router";
+import { useCurrentUser } from "@/features/auth/hooks/useCurrentUser";
+import { ChangeEmailDialog } from "@/features/auth/components/ChangeEmailDialog";
 
 export const Route = createFileRoute("/settings")({
-  component: SettingsPage,
-  loader: async ({ context: { trpcQueryUtils } }) => {
-    const { currentUser } = await trpcQueryUtils.auth.currentUser.ensureData();
+    component: SettingsPage,
+    loader: async ({ context: { trpcQueryUtils } }) => {
+        const { currentUser } = await trpcQueryUtils.auth.currentUser.ensureData();
 
-    if (!currentUser) {
-      return redirect({ to: "/login" });
-    }
-  },
+        if (!currentUser) {
+            return redirect({ to: "/login" });
+        }
+    },
 });
 
 function SettingsPage() {
-  const utils = trpc.useUtils();
-  const { toast } = useToast();
+    const utils = trpc.useUtils();
+    const { toast } = useToast();
+    const { currentUser } = useCurrentUser();
 
-  const logoutMutation = trpc.auth.logout.useMutation({
-    onSuccess: async () => {
-      await utils.auth.currentUser.invalidate();
+    const logoutMutation = trpc.auth.logout.useMutation({
+        onSuccess: async () => {
+            await utils.auth.currentUser.invalidate();
 
-      router.navigate({ to: "/login" });
+            router.navigate({ to: "/login" });
 
-      toast({
-        title: "Logged out",
-        description: "You have been logged out",
-      });
-    },
-    onError: (error) => {
-      toast({
-        title: "Error logging out",
-        description: error.message,
-        variant: "destructive",
-      });
-    },
-  });
+            toast({
+                title: "Logged out",
+                description: "You have been logged out",
+            });
+        },
+        onError: (error) => {
+            toast({
+                title: "Error logging out",
+                description: error.message,
+                variant: "destructive",
+            });
+        },
+    });
 
-  const settings = [
-    {
-      label: "Sign out of your account",
-      component: (
-        <Button
-          variant="destructive"
-          disabled={logoutMutation.isPending}
-          onClick={() => logoutMutation.mutate()}
-        >
-          {logoutMutation.isPending ? "Logging out..." : "Logout"}
-        </Button>
-      ),
-    },
-  ];
+    const settings = [
+        {
+            label: currentUser?.email,
+            component: <ChangeEmailDialog />,
+        },
 
-  return (
-    <main className="space-y-4">
-      {settings.map((setting) => (
-        <Card key={setting.label} className="flex items-center justify-between">
-          <span className="text-neutral-600 dark:text-neutral-400">
-            {setting.label}
-          </span>
-          {setting.component}
-        </Card>
-      ))}
-    </main>
-  );
+        {
+            label: "Sign out of your account",
+            component: (
+                <Button
+                    variant="destructive"
+                    disabled={logoutMutation.isPending}
+                    onClick={() => logoutMutation.mutate()}
+                >
+                    {logoutMutation.isPending ? "Logging out..." : "Logout"}
+                </Button>
+            ),
+        },
+    ];
+
+    return (
+        <main className="space-y-4">
+            {settings.map((setting) => (
+                <Card key={setting.label} className="flex items-center justify-between">
+                    <span className="text-neutral-600 dark:text-neutral-400">
+                        {setting.label}
+                    </span>
+                    {setting.component}
+                </Card>
+            ))}
+        </main>
+    );
 }
